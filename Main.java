0a736c78623ad9048587e8dfbef82e561c90f938
BighiAlessandro <alessandrobighi200@gmail.com>
2023-12-15 13:55:51 +0000

    continuo

--- Main.java
+++ Main.java
@@ -1,12 +1,29 @@
-import com.sun.swing.internal.plaf.metal.resources.metal_zh_CN;
+import java.util.Scanner;
 
 class Main {
   public static void main(String[] args) {
-    System.out.println("Cifriamo il nostro testo e trasformiamolo in un testo segreto!");
-  }
-
+  Scanner scanner = new Scanner(System.in);
+  String chiave = "chiave";
   Matrice m = new Matrice("TPSIT");
-  Vigenere V = new Vigenere(0, 12, 0, 12, m);
+  Vigenere v1 = new Vigenere(0, 12, 0, 12, m);
+  Vigenere v2 = new Vigenere(0, 12, 13, 25, m);
+  Vigenere v3 = new Vigenere(13, 25, 0, 12, m);
+  Vigenere v4 = new Vigenere(13, 25, 13, 25, m);
 
-  //commento di prova
-}
+  Thread t1 = new Thread(v1);
+  Thread t2 = new Thread(v2);
+  Thread t3 = new Thread(v3);
+  Thread t4 = new Thread(v4);  
+
+  t1.start();
+  t2.start();
+  t3.start();
+  t4.start();
+
+  System.out.println("Inserisci la frase in chiaro da poi: ");
+  String frase = scanner.nextline();
+  scanner.close();
+
+  String frase = m.()
+    }
+  }
